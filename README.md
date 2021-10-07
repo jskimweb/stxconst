cbdd788083e6c8112839d335f5536b3c9acf180f
jskimweb <jskimweb@gmail.com>
2021-10-07 10:59:46 +0900

    Update '.modal'

---
+++ README.md
@@ -0,0 +1,5 @@
+# STX건설 퍼블리싱
+
+퍼블리싱 : https://jskimweb.github.io/stxconst
+
+원사이트 : http://www.stxconst.co.kr
--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 window.onload = function () {
-    // modal
+    // 모달창 닫기
     const modal = $('.modal');
+    const modalBox = $('.modal-box');
+    const modalClose = $('.modal__close');
 
-    modal.click(function () {
-        $(this).fadeOut();
+    modalClose.click(function() {
+        modal.stop().fadeOut();
     });
 
-    // gnb hover
+    modal.click(function () {
+        $(this).stop().fadeOut();
+    });
+    
+    modalBox.click(function(event) {
+        event.stopPropagation();
+    });
+
+    // gnb 보이기/숨기기
     const mainMenu = $('.dep01>li');
     const header = $('.header');
     const openHeader = 'header--open';
@@ -17,7 +27,7 @@ window.onload = function () {
         header.removeClass(openHeader);
     });
 
-    // move to business
+    // 버튼 클릭 시 Business로 이동
     const goBusi = $('.go-busi');
     const busi = $('.busi');
 
